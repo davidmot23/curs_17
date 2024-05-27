81f63ae8bacdf41ed0c887562b439eded9234c06
unknown <filip.david.mot@gmail.com>
2024-05-27 19:29:10 +0300

    problema5

---
+++ main.py
@@ -0,0 +1,12 @@
+#5 Scrieti o functie care citeste un string de la tastatura si afiseaza lungimea lui. Tratati cazul in care textul nu este string
+def lungimestr():
+    try:
+        string=str(input("Introduceti un string: "))
+        print(len(string))
+    except ValueError:
+        print('Trebuie sa introduceti string')
+
+lungimestr()
+
+
+#6 Fiind dat un dictionar {"a":1,"b":2,"c":3} scrieti o functie care primeste ca parametru cheia si returneaza valoarea
